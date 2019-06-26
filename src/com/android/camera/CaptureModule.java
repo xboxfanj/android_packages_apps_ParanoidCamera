4cdceca221993bffceb0e8204048580a5e9af87c
xianming wang <mingwax@codeaurora.org>
2019-06-26 17:42:16 +0800

    SnapdragonCamera: Remove Zoom seekBar in Pro mode and RTB mode
    Remove seekBar in Pro mode and RTB mode.
    
    CRs-Fixed: 2479354
    Change-Id: I0fb67d3792f382eca52312cd4573f463ff3474b7

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -7833,14 +7833,18 @@ public class CaptureModule implements CameraModule, PhotoController,
         } else {
             restartAll();
         }
+        updateZoomSeekBarVisible();
+        mUI.updateZoomSeekBar(1.0f);
+        return 1;
+    }
+
+    public void updateZoomSeekBarVisible() {
         if (mCurrentSceneMode.mode == CameraMode.PRO_MODE ||
                 mCurrentSceneMode.mode == CameraMode.RTB) {
             mUI.hideZoomSeekBar();
         } else {
             mUI.showZoomSeekBar();
         }
-        mUI.updateZoomSeekBar(1.0f);
-        return 1;
     }
 
     public int getCurrentModeIndex() {
