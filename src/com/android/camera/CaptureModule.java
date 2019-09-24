3b1fc787eda0075727bb6b71ea51c0ad6878a533
xianming wang <mingwax@codeaurora.org>
2019-09-24 10:35:25 +0800

    SnapdragonCamera: Correct save cameraID when call onPause method
    Correct save CameraID when call onPause method.
    
    Change-Id: Icb128ddfee8406e03e7214656de3f7a00371ef52

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -7463,9 +7463,9 @@ public class CaptureModule implements CameraModule, PhotoController,
 
     public void restartAll() {
         Log.d(TAG, "restart all");
-        reinit();
         onPauseBeforeSuper();
         onPauseAfterSuper(false);
+        reinit();
         onResumeBeforeSuper();
         onResumeAfterSuper(true);
         setRefocusLastTaken(false);
