0b18834bd9e087faadac4174676e7b0d7f41ca75
junjiez <junjiez@codeaurora.org>
2019-10-28 11:18:30 +0800

    SnapdraongCamera:Storage not enough
    When recording failure caused by
    not enough storage, not need to
    release MediaRecorder.
    
    Change-Id: Ifbead3880374875670b8326368429c85060789de
    CRs-Fixed: 2549731

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -5067,6 +5067,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             Log.w(TAG, "Storage issue, ignore the start request");
             mStartRecPending = false;
             mIsRecordingVideo = false;
+            Toast.makeText(mActivity,R.string.storage_not_enough,Toast.LENGTH_SHORT).show();
             return false;
         }
 
@@ -6015,7 +6016,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             if (!startRecordingVideo(getMainCameraId())) {
                 // Show ui when start recording failed.
                 mUI.showUIafterRecording();
-                releaseMediaRecorder();
+                mFrameProcessor.setVideoOutputSurface(null);
             }
         } else if (mMediaRecorderStarted) {
             stopRecordingVideo(getMainCameraId());
