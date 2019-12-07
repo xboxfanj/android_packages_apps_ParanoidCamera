15f284f7f648d60b7a67c81b987ce0b85e576c9d
junjiez <junjiez@codeaurora.org>
2019-12-07 15:06:06 +0800

    SnapdraongCamera:Fix max preview
    choose product of max preview width
    and height as the limitation, and for
    1:1 size, choose max preview width as
    limitation.
    
    Change-Id: Ifcbddafbab6c655c23520314ce12e04912565a49

--- src/com/android/camera/util/CameraUtil.java
+++ src/com/android/camera/util/CameraUtil.java
@@ -617,6 +617,8 @@ public class CameraUtil {
         // Try to find an size match aspect ratio and size
         for (int i = 0; i < sizes.length; i++) {
             Point size = sizes[i];
+            if(size == null)
+                continue;
             double ratio = (double) size.x / size.y;
             if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
 
@@ -688,6 +690,8 @@ public class CameraUtil {
         // Try to find an size match aspect ratio and size
         for (int i = 0; i < sizes.length; i++) {
             Point size = sizes[i];
+            if (size == null)
+                continue;
             double ratio = (double) size.x / size.y;
             if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
 
