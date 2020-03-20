c46586ec738270a58043c8ad2031cc924986c201
jinwu <jinwu@codeaurora.org>
2020-03-20 13:41:18 +0800

    Make unlockFocus and enableShutter synchronized
    Change-Id: I5793d6ef08a92d9f8510189366b4e3032e81b73c

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -3096,10 +3096,13 @@ public class CaptureModule implements CameraModule, PhotoController,
                 public void onCaptureSequenceCompleted(CameraCaptureSession session, int
                         sequenceId, long frameNumber) {
                     Log.d(TAG, "captureStillPictureForCommon onCaptureSequenceCompleted: " + id);
-                    if (mUI.getCurrentProMode() != ProMode.MANUAL_MODE) {
-                        unlockFocus(id);
-                    } else {
-                        enableShutterAndVideoOnUiThread(id,false);
+                    if (!captureWaitImageReceive()) {
+                        if (mUI.getCurrentProMode() != ProMode.MANUAL_MODE) {
+                            unlockFocus(id);
+                        } else {
+                            enableShutterAndVideoOnUiThread(id,false);
+                        }
+                        Log.d(TAG,"onShutterButtonRelease");
                     }
                     if (mSettingsManager.isHeifWriterEncoding()) {
                         if (mHeifImage != null) {
@@ -3367,10 +3370,6 @@ public class CaptureModule implements CameraModule, PhotoController,
                                             }
                                         });
                                     }
-//                                    final Image image = reader.acquireNextImage();
-//                                    byte[] bytes = getYUVFromImage(image);
-//                                    mActivity.getMediaSaveService().addRawImage(bytes, title, "yuv");
-//                                    image.close();
                                 }
                             };
 
@@ -3392,6 +3391,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                                             mUI.enableShutter(true);
                                         }
                                     });
+                                    unlockFocus(mCamId);
                                 }
                                 Log.d(TAG, "image available for cam: " + mCamId);
                                 Image image = reader.acquireNextImage();
