90ed162f033fd9c1020f8a52cccf20c5e4499e62
Jake Weinstein <jake@aospa.co>
2020-05-03 00:52:07 +0200

    ParanoidCamera: Rename package to "co.aospa.camera".
    Change-Id: I7e62fd9ff5b0f9b6449e5cf143826e9733de5b69

--- Android.mk
+++ Android.mk
@@ -25,12 +25,14 @@ LOCAL_AAPT_FLAGS := \
         --version-name "$(version_name_package)" \
         --version-code $(version_code_package) \
 
-LOCAL_PACKAGE_NAME := SnapdragonCamera
+LOCAL_PACKAGE_NAME := ParanoidCamera
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRIVATE_PLATFORM_APIS:=true
 
 #LOCAL_SDK_VERSION := current
+LOCAL_AAPT_FLAGS += --rename-manifest-package co.aospa.camera
+
 LOCAL_RENDERSCRIPT_TARGET_API := 23
 
 #Do not override for targets using vanilla AOSP
